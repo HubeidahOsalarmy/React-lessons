12509a239cbfb189ff87001fb0e51088774fda3a
Hubeidahsalarmy67 <hubeidahsalarmy@gmail.com>
2023-08-09 20:51:37 +0000

    props and state

--- src/App.js
+++ src/App.js
@@ -3,15 +3,24 @@ import React, { Component } from "react";
 import "./App.css";
 import Greet from "./components/Greet"
 // import Hello from "./components/Hello"
-
+import Welcome from "./components/welcome"
+import Message from "./components/Message"
 
 class App extends Component {
   render() {
     return (
      <div className="App">
-       < Greet name="Murewa" heroName="Taiyelolu"/>
-       < Greet name="Mayowa" heroName="Omokehinde"/>
-       < Greet name="Muyiwa" heroName="Idowu"/>
+       <Message />
+       {/*<Greet name="Murewa" heroName="Ubaydullah">
+         <p>This is children props</p>
+       </Greet>
+       <Greet name="Mayowa" heroName="Abdullah">
+         <button>Bismillah</button>
+       </Greet>
+       < Greet name="Muyiwa" heroName="Abdulmajid"/>
+       <Welcome name="Murewa" heroName="Ubaydullah"/>
+       <Welcome name="Mayowa" heroName="Abdullah"/>
+       <Welcome name="Muyiwa" heroName="Abdulmajid"/>*/}
      </div>
     );
   }
