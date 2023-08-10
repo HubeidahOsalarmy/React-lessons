6ad6216eb32edf9b2ae0d0b6dcd5ea23762e402d
Hubeidahsalarmy67 <hubeidahsalarmy@gmail.com>
2023-08-10 19:44:32 +0000

    setState

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import Greet from "./components/Greet"
 // import Hello from "./components/Hello"
 import Welcome from "./components/welcome"
 import Message from "./components/Message"
+import Counter from "./components/Counter"
 
 class App extends Component {
   render() {
     return (
      <div className="App">
-       <Message />
+        <Counter />
+        { /* <Message /> */}
        {/*<Greet name="Murewa" heroName="Ubaydullah">
          <p>This is children props</p>
        </Greet>
